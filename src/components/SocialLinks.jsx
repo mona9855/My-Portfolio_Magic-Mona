9e14c69282acb261f61de01095a6e8eed7f0415e
mona9855 <mona.magdy2012912012911@gmail.com>
2024-02-27 20:14:11 +0200

    social links style added

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -2,24 +2,53 @@ import React from "react";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
 import { SiUpwork } from "react-icons/si";
+import { TbLetterW } from "react-icons/tb";
 
 const SocialLinks = () => {
   return (
-    <div className="flex gap-3 mt-2">
+    <div className="flex gap-3 mt-1">
       <a
         href="https://www.linkedin.com/in/mona-magdy-300b03234/"
         target="_blank"
         rel="noreferrer"
+        title="Linkedin"
+        className="p-1 rounded bg-blue-500" 
       >
-        <FaLinkedin size={30} color="#126bc4" className="cursor-pointer" />
+        <FaLinkedin size={30} color="white" className="cursor-pointer" />
       </a>
 
-      <a href="https://github.com/mona9855" target="_blank" rel="noreferrer">
-        <FaGithub size={30} color="black" className="cursor-pointer" />
+      <a
+        href="https://github.com/mona9855"
+        target="_blank"
+        rel="noreferrer"
+        title="GitHub"
+        className="p-1 rounded bg-black"
+      >
+        <FaGithub size={30} color="white" className="cursor-pointer" />
       </a>
 
-      <a href="https://www.upwork.com/freelancers/~014d11d9c795ef93e1" target="_blank" rel="noreferrer">
-        <SiUpwork size={30} color="green" className="cursor-pointer" />
+      <a
+        href="https://www.upwork.com/freelancers/~014d11d9c795ef93e1"
+        target="_blank"
+        rel="noreferrer"
+        title="Upwork"
+        className="p-1 rounded bg-green-700"
+      >
+        <SiUpwork size={30} color="white" className="cursor-pointer" />
+      </a>
+
+      <a
+        href="https://wuzzuf.net/me/mona-magdy-b0eb2207a6?utm_medium=other&utm_source=referral"
+        target="_blank"
+        rel="noreferrer"
+        className="p-1 rounded bg-blue-900"
+      >
+        <TbLetterW
+          size={30}
+          color="white"
+          className="cursor-pointer"
+          title="Wuzzuf"
+        />
       </a>
     </div>
   );
