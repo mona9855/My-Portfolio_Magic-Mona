873b671b3643ca5eb6005502d2352c48beed46e5
mona9855 <mona.magdy2012912012911@gmail.com>
2024-07-10 04:33:51 +0300

    updating the code

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,48 +1,67 @@
-import css from './css.svg'
-import git from './git.svg'
-import github from './github.svg'
-import html from './html.svg'
-import javascript from './javascript.svg'
-import motion from './motion.svg'
-import react from './react.svg'
-import redux from './redux.svg'
-import sass from './sass.svg'
-import tailwindcss from './tailwindcss.svg'
-import linkedin from './linkedin.svg'
-import twitter from './twitter.svg'
-import contact from './contact.svg'
-import soundon from './soundon.png'
-import soundoff from './soundoff.png'
-import shirt from './shirt.png'
-import gpt from './gpt.png'
-import gym from './gym.svg'
-import arrow from './arrow.svg'
-import company1 from '../icons/company1.jpeg'
-import freelancer from '../icons/freelancer.png'
-import university from '../icons/university.jpg'
-
+import css from "./css.svg";
+import git from "./git.svg";
+import github from "./github.svg";
+import html from "./html.svg";
+import javascript from "./javascript.svg";
+import motion from "./motion.svg";
+import react from "./react.svg";
+import redux from "./redux.svg";
+import sass from "./sass.svg";
+import tailwindcss from "./tailwindcss.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import contact from "./contact.svg";
+import soundon from "./soundon.png";
+import soundoff from "./soundoff.png";
+import shirt from "./shirt.png";
+import gpt from "./gpt.png";
+import gym from "./gym.svg";
+import arrow from "./arrow.svg";
+import logicDesigns from "../icons/company1.jpeg";
+import freelancer from "../icons/freelancer.png";
+import university from "../icons/university.jpg";
+import vigorant from "../icons/vigorant.jpg";
+import flexiApps from "../icons/flexiApps.png";
+import next from "../icons/next.jpeg";
+import figma from "../icons/figma.png";
+import bootstrap from "../icons/bootstrap.png";
+import jquery from "../icons/jquery.png";
+import docker from "../icons/docker.png";
+import postman from "../icons/postman.svg";
+import c from "../icons/c.png";
+import machineLanguage from "../icons/machineLanguage.png";
 
 export {
-    university,
-    company1,
-    freelancer,
-    shirt,
-    gpt,
-    gym,
-    arrow,
-    css,
-    git,
-    github,
-    html,
-    javascript,
-    motion,
-    react,
-    redux,
-    sass,
-    tailwindcss,
-    linkedin,
-    twitter,
-    contact,
-    soundon,
-    soundoff
-}
+  machineLanguage,
+  c,
+  postman,
+  bootstrap,
+  jquery,
+  docker,
+  next,
+  figma,
+  flexiApps,
+  vigorant,
+  university,
+  logicDesigns,
+  freelancer,
+  shirt,
+  gpt,
+  gym,
+  arrow,
+  css,
+  git,
+  github,
+  html,
+  javascript,
+  motion,
+  react,
+  redux,
+  sass,
+  tailwindcss,
+  linkedin,
+  twitter,
+  contact,
+  soundon,
+  soundoff,
+};
